9d9fe159f98b6b09f2e869068b9708536d2d2ecc
EC2 Default User <ec2-user@ip-172-31-35-173.ap-northeast-1.compute.internal>
2023-08-12 00:37:14 +0000

    make table

---
+++ db/migrate/20230812000721_create_orders.rb
@@ -0,0 +1,13 @@
+class CreateOrders < ActiveRecord::Migration[6.1]
+  def change
+    create_table :orders do |t|
+      t.integer :amount_billed, null: false
+      t.string :address, null: false
+      t.string :name, null: false
+      t.string :post_number, null: false
+      t.integer :postage, null: false, default: "800"
+      t.integer :payment_method, null: false
+      t.timestamps
+    end
+  end
+end
