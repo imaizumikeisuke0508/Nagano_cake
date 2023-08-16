6a390dcc7b7987b1a195ecf599cf0275d50c19b5
keisukeimaizumi <keisuke115826vy@gmail.com>
2023-08-16 13:16:52 +0000

    fix

--- config/routes.rb
+++ config/routes.rb
@@ -13,7 +13,7 @@ devise_for :customers,skip: [:passwords], controllers: {
 devise_for :admin, skip:[:registrations, :passwods], controllers: {
   sessions: "admin/sessions"
 }
-  namespace :admin do
-   resources :orders, only: [:index, :show]
-　end
+namespace :admin do
+  resources :orders, only: [:index, :show]
+  end
 end
